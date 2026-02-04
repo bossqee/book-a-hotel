c1e680ca1259a693572c1d860bf34ec7d00bd957
bossqee <p30675130@gmaiil.com>
2026-02-04 11:56:35 +0700

    #

--- project-react/src/layout/Layout.jsx
+++ project-react/src/layout/Layout.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import {Outlet} from 'react-router'
-import Nav_Home from '../components/ui/Nav_Home.jsx'
+import Nav_Main from '../components/ui/Nav_Main.jsx'
 
 const Layout = () => {
   return (
     <div>
-      <Nav_Home />
+      <Nav_Main />
       <Outlet />
     </div>
   )
--- project-react/src/page/Home.jsx
+++ project-react/src/page/Home.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import '../App.css';
-import Nav_Home from '../components/ui/Nav_Home';
+import React from "react";
+import "../App.css";
+import Nav_Main from "../components/ui/Nav_Main.jsx";
 
 const Home = () => {
   return (
-    <div className='absolute inset-0 bg-cover bg-center home-background'>
-        <Nav_Home />
+    <div className="absolute inset-0 bg-cover bg-center home-background">
+      <Nav_Main />
+      <div className="flex justify-center items-center h-screen flex-col gap-4 text-center px-4">
+        <h1 className="text-4xl font-bold title-white">Your Gateway to the</h1>
+        <h1 className="text-4xl font-bold title-yellow">Extraordinary</h1>
+        <p className="title-p">
+          สัมผัสประสบการณ์ความหรูหราและความสะดวกสบายอย่างที่ไม่เคยมีมาก่อน —
+          การเข้าพักที่สมบูรณ์แบบของคุณเริ่มต้นที่นี่
+        </p>
+        <form action="">
+          <input
+            type="text"
+            className="search-input outline w-100 h-12 rounded-md transition-all shadow-lg text-white p-4"
+            placeholder="Search for hotels..."
+          />
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
